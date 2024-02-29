e35ec7aa3c04de4c3b5ba4841bd7e4b8af08b0b2
AleGxrcia <fagarciatavarez@gmail.com>
2024-02-29 19:29:08 -0400

    Configure AutoMapper for ViewModels and DTOs

---
+++ SocialNetwork.Core.Application/Mappings/GeneralProfile.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using SocialNetwork.Core.Application.Dtos.Account;
+using SocialNetwork.Core.Application.ViewModels.User;
+
+namespace SocialNetwork.Core.Application.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            #region User
+            CreateMap<AuthenticationRequest, LoginViewModel>()
+                .ForMember(x => x.HasError, opt => opt.Ignore())
+                .ForMember(x => x.Error, opt => opt.Ignore())
+                .ReverseMap();
+
+            CreateMap<RegisterRequest, SaveUserViewModel>()
+                .ForMember(x => x.File, opt => opt.Ignore())
+                .ForMember(x => x.HasError, opt => opt.Ignore())
+                .ForMember(x => x.Error, opt => opt.Ignore())
+                .ReverseMap();
+
+            CreateMap<ForgotPasswordRequest, ForgotPasswordViewModel>()
+                .ForMember(x => x.HasError, opt => opt.Ignore())
+                .ForMember(x => x.Error, opt => opt.Ignore())
+                .ReverseMap();
+
+            CreateMap<ResetPasswordRequest, ResetPasswordViewModel>()
+                .ForMember(x => x.HasError, opt => opt.Ignore())
+                .ForMember(x => x.Error, opt => opt.Ignore())
+                .ReverseMap();
+            #endregion
+        }
+    }
+}
