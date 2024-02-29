77080b940df5193cc0882c69589d9fd2675d093c
AleGxrcia <fagarciatavarez@gmail.com>
2024-02-29 19:36:58 -0400

    Add service registration and package installation

---
+++ SocialNetwork.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SocialNetwork.Infrastructure.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddIdentityInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+
+        }
+    }
+}
