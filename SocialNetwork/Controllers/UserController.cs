36b34c7f39e923203b0ff4be555f2d097aaee1ab
AleGxrcia <fagarciatavarez@gmail.com>
2024-02-29 19:39:20 -0400

    Add UserController with user authentication and management endpoints

---
+++ SocialNetwork/Controllers/UserController.cs
@@ -0,0 +1,188 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Core.Application.Dtos.Account;
+using SocialNetwork.Core.Application.Interfaces.Services;
+using SocialNetwork.Core.Application.ViewModels.User;
+using SocialNetwork.Core.Application.Helpers;
+
+namespace WebApp.SocialNetwork.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IActionResult Login()
+        {
+            return View(new LoginViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            AuthenticationResponse userVm = await _userService.LoginAsync(vm);
+            if (userVm != null && userVm.HasError != true)
+            {
+                HttpContext.Session.Set<AuthenticationResponse>("user", userVm);
+                return RedirectToRoute(new { controller = "Home", action = "Index" });
+            }
+            else
+            {
+                vm.HasError = userVm.HasError;
+                vm.Error = userVm.Error;
+                return View(vm);
+            }
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _userService.SignOutAsync();
+            HttpContext.Session.Remove("user");
+            return RedirectToRoute(new { controller = "User", action = "Login" });
+        }
+
+        public IActionResult Register()
+        {
+            return View(new SaveUserViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(SaveUserViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var origin = Request.Headers["origin"];
+            RegisterResponse response = await _userService.RegisterAsync(vm, origin);
+            if (response.HasError)
+            {
+                vm.HasError = response.HasError;
+                vm.Error = response.Error;
+                return View(vm);
+            }
+
+            if (response.Id != null)
+            {
+                vm.ProfilePicture = UploadFile(vm.File, response.Id);
+                await _userService.UpdateAsync(vm, response.Id);
+            }
+
+            return RedirectToRoute(new { controller = "User", action = "Login" });
+        }
+
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            string response = await _userService.ConfirmEmailAsync(userId, token);
+            return View("ConfirmEmail", response);
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View(new ForgotPasswordViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var origin = Request.Headers["origin"];
+            ForgotPasswordResponse response = await _userService.ForgotPasswordAsync(vm, origin);
+
+            if (response.HasError)
+            {
+                vm.HasError = response.HasError;
+                vm.Error = response.Error;
+                return View(vm);
+            }
+
+            return RedirectToRoute(new { controller = "User", action = "Login" });
+        }
+
+        public IActionResult ResetPassword(string token)
+        {
+            return View(new ResetPasswordViewModel { Token = token });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            ResetPasswordResponse response = await _userService.ResetPasswordAsync(vm);
+            if (response.HasError)
+            {
+                vm.HasError = response.HasError;
+                vm.Error = response.Error;
+                return View(vm);
+            }
+
+            return RedirectToRoute(new { controller = "User", action = "Login" });
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+        private string UploadFile(IFormFile file, string id, bool isEditMode = false, string imagePath = "")
+        {
+            if (isEditMode && file == null)
+            {
+                return imagePath;
+            }
+
+            //get directory path
+            string basePath = $"/Images/Users/{id}";
+            string path = Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot{basePath}");
+
+            //create folder if no exist
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            //get file path
+            Guid guid = Guid.NewGuid();
+            FileInfo fileInfo = new(file.FileName);
+            string filename = guid + fileInfo.Extension;
+
+            string fileNameWithPath = Path.Combine(path, filename);
+
+            using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
+            if (isEditMode)
+            {
+                string[] oldImagePart = imagePath.Split("/");
+                string oldImageName = oldImagePart[^1];
+                string completeImageOldPath = Path.Combine(path, oldImageName);
+
+                if (System.IO.File.Exists(completeImageOldPath))
+                {
+                    System.IO.File.Delete(completeImageOldPath);
+                }
+            }
+
+            return $"{basePath}/{filename}";
+        }
+    }
+}
