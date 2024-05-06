6ec339840424ba4649a0040ef65111d1de80578f
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-05-06 17:22:06 -0400

    add entity repositories

--- SocialNetwork.Infrastructure.Persistence/ServiceRegistration.cs
+++ SocialNetwork.Infrastructure.Persistence/ServiceRegistration.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
-﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SocialNetwork.Core.Application.Interfaces.Repositories;
+using SocialNetwork.Infrastructure.Persistence.Contexts;
+using SocialNetwork.Infrastructure.Persistence.Repositories;
 
 namespace SocialNetwork.Infrastructure.Persistence
 {
@@ -14,13 +17,20 @@ namespace SocialNetwork.Infrastructure.Persistence
                 services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("AppDb"));
             }
             else
-        {
+            {
                 services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(config.GetConnectionString("DefaultConnection"),
                 m => m.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
             }
             #endregion
 
+            #region Repositories
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddTransient<IPostRepository, PostRepository>();
+            services.AddTransient<ICommentRepository, CommentRepository>();
+            services.AddTransient<IReplyRepository, ReplyRepository>();
+            services.AddTransient<IFriendRepository, FriendRepository>();            
+            #endregion
         }
     }
 }
