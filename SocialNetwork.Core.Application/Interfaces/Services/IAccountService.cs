ee087b7ce8ece97f8df7d2179ad50a3f49c1ba21
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-05-06 17:23:12 -0400

    add entity services

--- SocialNetwork.Core.Application/Interfaces/Services/IAccountService.cs
+++ SocialNetwork.Core.Application/Interfaces/Services/IAccountService.cs
@@ -7,6 +7,8 @@ namespace SocialNetwork.Core.Application.Interfaces.Services
         Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request);
         Task<string> ConfirmAccountAsync(string userId, string token);
         Task<ForgotPasswordResponse> ForgotPasswordAsync(ForgotPasswordRequest request, string origin);
+        Task<AuthenticationResponse> GetUserByIdAsync(string id);
+        Task<AuthenticationResponse> GetUserByUsernameAsync(string username);
         Task<RegisterResponse> RegisterUserAsync(RegisterRequest request, string origin);
         Task<ResetPasswordResponse> ResetPasswordAsync(ResetPasswordRequest request);
         Task SignOutAsync();
--- SocialNetwork.Core.Application/ServiceRegistration.cs
+++ SocialNetwork.Core.Application/ServiceRegistration.cs
@@ -11,7 +11,12 @@ namespace SocialNetwork.Core.Application
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
             #region Services
+            services.AddTransient(typeof(IGenericService<,,>), typeof(GenericService<,,>));
             services.AddTransient<IUserService, UserService>();
+            services.AddTransient<IPostService, PostService>();
+            services.AddTransient<IFriendService, FriendService>();
+            services.AddTransient<ICommentService, CommentService>();
+            services.AddTransient<IReplyService, ReplyService>();
             #endregion
         }
     }
