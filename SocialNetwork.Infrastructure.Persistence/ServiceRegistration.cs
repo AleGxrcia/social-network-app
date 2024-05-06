2eb7bce42ac80bf675c157692f1d84586fd44262
AleGxrcia <125775730+AleGxrcia@users.noreply.github.com>
2024-05-06 16:31:41 -0400

    Configure entities using Entity Framework Core Fluent API

--- SocialNetwork.Infrastructure.Persistence/ServiceRegistration.cs
+++ SocialNetwork.Infrastructure.Persistence/ServiceRegistration.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.EntityFrameworkCore;
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -5,8 +6,20 @@ namespace SocialNetwork.Infrastructure.Persistence
 {
     public static class ServiceRegistration
     {
-        public static void AddIdentityInfrastructure(this IServiceCollection services, IConfiguration config)
+        public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration config)
         {
+            #region "Context configurations"
+            if (config.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("AppDb"));
+            }
+            else
+        {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection"),
+                m => m.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            }
+            #endregion
 
         }
     }
